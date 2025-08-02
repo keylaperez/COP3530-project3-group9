0a3b49113754f65754ed9dc0eec5a3b9348db8a9
syd000 <sfung0323.college@gmail.com>
2025-08-02 14:49:43 -0400

    Added Trie class into cmake

--- movieTrie.h
+++ movieTrie.h
@@ -5,10 +5,22 @@
 #ifndef MOVIETRIE_H
 #define MOVIETRIE_H
 
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
 
 class movieTrie {
+private:
+    unordered_map<string,vector<string>> movies;
 
+public:
+    void insertMovie(string name, string year, string genre, string rating);
+    void titleSearch(string name);
+    void yearFilter(string year);
+    void genreFilter(string genre);
+    void ratingByGenre(string genre);
 };
 
 
