22657309c89ed5ad127ec99a4bb8d01a1c387f11
Keyla <Keyla_1811@yahoo.com>
2025-08-02 13:52:00 -0400

    Took the spaces out from the choices in the main menu

--- main.cpp
+++ main.cpp
@@ -9,13 +9,12 @@ using namespace std;
 void mainMenu() {
   cout << "\n === Welcome to the Movie Recommender! === \n"
           "\n Please select an option from the following menu: \n"
-          "\n 1. Movie Title \n"
-          "\n 2. Release Year \n"
-          "\n 3. Movie Genre\n"
-          "\n 4. Rating by Genre \n";
+          " 1. Movie Title \n"
+          " 2. Release Year \n"
+          " 3. Movie Genre\n"
+          " 4. Rating by Genre \n"
+          " 5. EXIT \n";
   }
-
-
 
 int main() {
   mainMenu();
