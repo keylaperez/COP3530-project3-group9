96eedac89422ed35af2782cf27f48de330233f8e
Keyla <Keyla_1811@yahoo.com>
2025-08-02 13:10:54 -0400

    added the main menu options

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
 
 using namespace std;
 
+
+void mainMenu() {
+  cout << "\n === Welcome to the Movie Recommender! === \n"
+          "\n Please select an option from the following menu: \n"
+          "\n 1. Movie Title \n"
+          "\n 2. Release Year \n"
+          "\n 3. Movie Genre\n"
+          "\n 4. Rating by Genre \n";
+  }
+
+
+
 int main() {
-  cout << "movie recommender" << endl;
+  mainMenu();
   return 0;
 }
