df711bc35d06da65257167e3f915b78df70017c0
w1nn1weng <zweng@ufl.edu>
2025-08-02 14:32:58 -0400

    updated movieHashMap functions and cleaned up CMakeList

--- movieHashMap.h
+++ movieHashMap.h
@@ -5,8 +5,23 @@
 #ifndef COP3530_PROJECT3_GROUP9_MOVIEHASHMAP_H
 #define COP3530_PROJECT3_GROUP9_MOVIEHASHMAP_H
 
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
 
 class movieHashMap {
+
+private:
+    unordered_map<string,vector<string>> movies;
+
+public:
+    void insertMovie(string name, string year, string genre, string rating);
+    void titleSearch(string name);
+    void yearFilter(string year);
+    void genreFilter(string genre);
+    void ratingByGenre(string genre);
 
 };
 
