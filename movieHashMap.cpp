f9eb028e440d8aa83d07d0b532b12e6c7f8b8925
w1nn1weng <zweng@ufl.edu>
2025-08-02 14:39:23 -0400

    updated .cpp for MovieHashMap with functions

--- movieHashMap.cpp
+++ movieHashMap.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "movieHashMap.h"
+using namespace std;
+
+void movieHashMap::genreFilter(std::string genre) {
+
+}
+
+void movieHashMap::insertMovie(std::string name, std::string year, std::string genre, std::string rating) {
+
+}
+
+void movieHashMap::ratingByGenre(std::string genre) {
+
+}
+
+void movieHashMap::titleSearch(std::string name) {
+
+}
+
+void movieHashMap::yearFilter(std::string year) {
+
+}
