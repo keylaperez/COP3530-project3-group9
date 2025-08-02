1a60d51dab10ae1131c023f32075a550fd3ffe15
Keyla <Keyla_1811@yahoo.com>
2025-08-02 13:58:56 -0400

    added a way to read the numerical choice from the menu [lines 12-17]

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 // Created by Keyla Perez on 8/2/25.
 //
 #include <iostream>
+#include <limits>
+#include <vector>
+#include "movieHashMap.h"
 
 using namespace std;
 
+//read the choice from the menu
+int getChoice() {
+    int choice;
+    if (!(cin >> choice)) {
+        cin.clear();
+        choice = -1;
+    }
+}
 
 void mainMenu() {
   cout << "\n === Welcome to the Movie Recommender! === \n"
@@ -17,6 +28,10 @@ void mainMenu() {
   }
 
 int main() {
-  mainMenu();
+    int choice;
+    do {
+        mainMenu();
+        choice = getChoice();
+    }
   return 0;
 }
