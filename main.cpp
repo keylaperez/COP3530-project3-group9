ede6833e47c19752cc3a1cc16cce2dc73e01986b
Keyla <Keyla_1811@yahoo.com>
2025-08-03 21:57:19 -0400

    took away option 4 from the menu since it is similar to option 3

--- main.cpp
+++ main.cpp
@@ -37,28 +37,6 @@ void mainMenu() {
 int main() {
 movieHashMap movieHashMap;
 
-    ifstream in("resources/movieHashMap.txt");
-
-    string line;
-    getline(in, line);
-
-    while (getline(in, line)) {
-        istringstream iss(line);
-        string title, year, yearGenre, rating, genre;
-
-        getline(iss, title, ',');
-        getline(iss, year, ',');
-        getline(iss, yearGenre, ',');
-        getline(iss, rating, ',');
-
-        int year = stoi(year);
-        int rate = stoi(rating);
-
-        movieHashMap.insertMovie(title, year, yearGenre, rate);
-    }
-
-    in.close();
-
     movieHashMap.insertMovie("Inception", 2010, "Sci-fi",      9);
     movieHashMap.insertMovie("Titanic", 1997, "Drama-Romance",  8);
     movieHashMap.insertMovie("Parastite", 2019, "Thriller-Drama",8);
@@ -90,13 +68,6 @@ movieHashMap movieHashMap;
                 string genre;
                 cin >> genre;
                 movieHashMap.genreFilter(genre);
-                break;
-            }
-            case 4: {
-                cout << "Enter Rating by Genre: ";
-                string genre;
-                cin >> genre;
-                movieHashMap.ratingByGenre(genre);
                 break;
             }
             case 5:
